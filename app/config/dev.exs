e67d7463eb57afd6eea86de942db432eca0950b3
im <poson_hemp@yahoo.co.jp>
2022-03-18 20:30:16 +0900

    connect Postgres port 15432 (before mix ecto.create)

--- app/config/dev.exs
+++ app/config/dev.exs
@@ -3,9 +3,10 @@ import Config
 # Configure your database
 config :my_app, MyApp.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "password",
   hostname: "localhost",
-  database: "my_app_dev",
+  # hostname: "db",
+  database: "testdb",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
@@ -18,7 +19,7 @@ config :my_app, MyApp.Repo,
 config :my_app, MyAppWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
